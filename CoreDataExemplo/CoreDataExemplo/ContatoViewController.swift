10c3f05ffe3211ef398dc1826d880f788feb2e19
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2022-09-13 12:04:24 -0300

    Initial commit

---
+++ CoreDataExemplo/CoreDataExemplo/ContatoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ContatoViewController.swift
+//  CoreDataExemplo
+//
+//  Created by Usuário Convidado on 13/09/22.
+//
+
+import UIKit
+
+class ContatoViewController: UIViewController {
+
+    @IBOutlet weak var txtNome: UITextField!
+    @IBOutlet weak var txtTelefone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func salvar(_ sender: Any) {
+    
+    }    
+}
