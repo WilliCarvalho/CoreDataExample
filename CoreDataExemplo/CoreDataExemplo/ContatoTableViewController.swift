da19512c59d69819845d6ddeffe26fff5e0c5c62
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2022-09-20 09:57:30 -0300

    showing core data on screen via tableView

--- CoreDataExemplo/CoreDataExemplo/ContatoTableViewController.swift
+++ CoreDataExemplo/CoreDataExemplo/ContatoTableViewController.swift
@@ -6,84 +6,55 @@
 //
 
 import UIKit
+import CoreData
 
 class ContatoTableViewController: UITableViewController {
 
+    var pessoas: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
-    // MARK: - Table view data source
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Pessoa")
+        
+        fetchRequest.sortDescriptors = [NSSortDescriptor.init(key: "nome", ascending: true)]
+        
+        do {
+            pessoas = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Deu ruim: \(error), \(error.userInfo)")
+        }
+        
+        self.tableView.reloadData()
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return pessoas.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let pessoa = pessoas[indexPath.row]
+        
+        cell.textLabel?.text = pessoa.value(forKeyPath: "nome") as? String
+        cell.detailTextLabel?.text = pessoa.value(forKey: "email" ) as? String
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- CoreDataExemplo/CoreDataExemplo/ContatoViewController.swift
+++ CoreDataExemplo/CoreDataExemplo/ContatoViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ContatoViewController: UIViewController {
 
@@ -20,6 +21,28 @@ class ContatoViewController: UIViewController {
     }
     
     @IBAction func salvar(_ sender: Any) {
+        self.save(name: txtNome.text!, telefone: txtTelefone.text!, email: txtEmail.text!)
+        
+        self.navigationController?.popViewController(animated: true)
+    }
     
-    }    
+    func save(name: String, telefone: String, email:String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Pessoa", in: managedContext)!
+        
+        let pessoa = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        pessoa.setValue(name, forKeyPath: "nome")
+        pessoa.setValue(telefone, forKeyPath: "telefone")
+        pessoa.setValue(email, forKeyPath: "email")
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Não foi possível salvar. \(error), \(error.userInfo)")
+        }
+    }
 }
